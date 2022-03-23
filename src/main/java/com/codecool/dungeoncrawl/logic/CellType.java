d3bc8ced910a5aca6ec0221d9822c9a60bca1c59
Péter Markó <peter.marko1089@gmail.com>
2022-03-23 12:01:30 +0100

    feat: new CellType

--- src/main/java/com/codecool/dungeoncrawl/logic/CellType.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/CellType.java
@@ -1,14 +1,18 @@
 package com.codecool.dungeoncrawl.logic;
 
 public enum CellType {
-    EMPTY("empty"),
-    FLOOR("floor"),
-    WALL("wall");
+    EMPTY("empty", null),
+    FLOOR("floor",null),
+    WALL("wall",null),
+    DOOR("door","brownkey");
 
     private final String tileName;
+    private final String itemFunction;
 
-    CellType(String tileName) {
+    CellType(String tileName, String itemFunction) {
         this.tileName = tileName;
+        this.itemFunction = itemFunction;
+
     }
 
     public String getTileName() {
